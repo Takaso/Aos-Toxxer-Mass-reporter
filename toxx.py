c41e202e4a310c001da8f7e0db2163eca5a4d7b8
Takaso <70639611+Takaso@users.noreply.github.com>
2021-06-02 19:21:29 +0200

    Update toxx.py

--- toxx.py
+++ toxx.py
@@ -28,7 +28,7 @@ print("""
 
 """)
 print("Made by %sTakaso%s and fixed by %sGlobal Occult Coalition%s" % (red(), reset(), blue(), reset()))
-print("%s\nReasons:%s\n%s0 = Illegal Content\n1 = Harassment\n2 = Spam or phishing links\n3 = Self Harm\n4 = NSFW Content%s\n%sYou have to choose one of these numbers as reason!%s" % (white(), reset(), magenta(), reset(), white(), reset()))
+print("%s\nReasons:%s\n%s0 = Illegal Content\n1 = Harassment\n2 = Spam or phishing links\n3 = Self Harm\n4 = NSFW Content%s\n%s You have to choose one of these numbers as reason!%s" % (white(), reset(), magenta(), reset(), white(), reset()))
 
 
 chanid = input("\n%sInsert channel ID: %s" % (blue(), reset()))
@@ -38,8 +38,8 @@ reason = input("%sChoose the reason: %s" % (blue(), reset()))
 numbersoftimes =  int(input("%sSelect the number of reports to do with each account: %s" % (blue(), reset())))
 
 def report():
-    with open("tokens.txt", "r") as tokens:
-        slaves = tokens.readlines()
+    with open("tokens.txt", "r", encoding="UTF-8") as tokens:
+        slaves = tokens.read().splitlines()
         for niggers in slaves:
             header = {
                 "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.305 Chrome/69.0.3497.128 Electron/4.0.8 Safari/537.36",
@@ -59,4 +59,4 @@ def report():
                     print ("%s Failed to report. %s" % (red(), reset()))
 
 report()
-end = input("\n\n%sReporting is done, if you want it to be longer add more tokens, press any key to exit: %s" % (cyan(), reset())) 
+end = input("\n\n%sReporting is done, if you want it to be longer add more tokens, press any key to exit: %s" % (cyan(), reset()))
